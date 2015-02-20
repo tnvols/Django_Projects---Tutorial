c8bb428b81dafb0c881f7d51170d0b9b8a5d3108
Nathan Sexton <nathan.sexton@getbellhops.com>
2015-02-20 16:47:19 -0500

    finished tutorial

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Choice, Question
 # Create your views here.
@@ -12,18 +13,32 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		"""Return the last five published questions. (not including
+			those set to be published in the future).
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
 	model = Question
 	template_name = 'polls/results.html'
+
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
@@ -31,7 +46,7 @@ def vote(request, question_id):
 	try:
 		selected_choice = p.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
-		#Redisplay the question voting form.
+		# Redisplay the question voting form.
 		return render(request, 'polls/detail.html', {
 			'question': p,
 			'error_message': "You didn't select a choice.",
